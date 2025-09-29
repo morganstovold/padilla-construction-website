566216014ad1dc8c6c276bfe02c451379223e303
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-09-29 15:25:42 -0700

    style: remove text-white class from navbar links to inherit parent color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,8 +39,8 @@ export default function Navbar() {
       className={buttonVariants({
         variant: "link",
         className: mobile
-          ? "group !no-underline relative justify-start p-0 font-medium text-lg text-white tracking-wide hover:text-tertiary"
-          : "group !no-underline relative whitespace-nowrap font-medium text-base text-shadow-lg text-white tracking-wide hover:text-tertiary",
+          ? "group !no-underline relative justify-start p-0 font-medium text-lg tracking-wide hover:text-tertiary"
+          : "group !no-underline relative whitespace-nowrap font-medium text-base text-shadow-lg tracking-wide hover:text-tertiary",
       })}
     >
       <div className="absolute bottom-0 left-0 h-px w-0 bg-tertiary transition-all duration-300 group-hover:w-full" />
